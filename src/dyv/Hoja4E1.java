e186f24f709279171e9f55ee41579caaa233b90a
rivara <ricardo.v.ramirez@gmail.com>
2016-02-26 23:35:18 +0100

    nueva version

--- src/dyv/Hoja4E1.java
+++ src/dyv/Hoja4E1.java
@@ -6,30 +6,33 @@ public class Hoja4E1 {
 			if (ini == fin) {
 				return true;
 			} else {
-
+				
 				int medio = (ini + fin) / 2;
-				Orden(numeros, ini, medio);
+				
+				if ((numeros[medio] > numeros[medio - 1])
+						&& (numeros[medio] < numeros[medio + 1])) {
+							 return true;
+			}else{
 				Orden(numeros, medio + 1, fin);
+				Orden(numeros, ini, medio);
+				
 
 				// si el numero por la izquierda es menor y por la derecha que el actual
-
-					if ((numeros[medio] > numeros[medio - 1])
-							&& (numeros[medio] < numeros[medio + 1])) {
-								 System.out.println("punto medio real"+medio);
-								 return true;
-				}
-					
-				return false;
+				
+				
+			}	
+			
 			}
+			return false;
 			
 		}
-
+//falla en la segunda mitad
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				// llamada principal
-				int arrayEntrada[] = {1,2,3,4,5};
+				int arrayEntrada[] = {1,4,5,7,20,11};
 				boolean ordenado = Orden(arrayEntrada, 1, arrayEntrada.length-1);
-				System.out.println("esta ordenado" + ordenado);
+				System.out.println("esta ordenado: " + ordenado);
 
 	}
 
