0b044dc4555e0c35ed2ccb1477f641270fdfdfe7
rivara <ricardo.v.ramirez@gmail.com>
2016-02-27 11:34:40 +0100

    Añadidos 2 nuevos ejercicios implementados por jesús

---
+++ src/dyv/MaximoMinimo.java
@@ -0,0 +1,59 @@
+package dyv;
+
+class MaximoMinimo
+{
+ public static void main(String[] args)
+ {
+	int arrayEntrada[]={ 1, 4, 5, -6, 123, 234, 213}; 
+	
+	int resultados[] = new int [2];
+	mym (arrayEntrada, resultados);
+	
+	System.out.println("Maximo: "+resultados[0]);
+	System.out.println("Minimo: "+resultados[1]);
+	  
+ }
+ 
+ 
+	static void mym (int a[], int []result) {
+		if (a.length>1) {
+			int [] result_1 = new int [2];
+			int [] result_2 = new int [2];
+			
+			int n = a.length /2;
+			int n2 = a.length - n;
+			int aux1 [] = new int [n];
+			int aux2 [] = new int [n2];
+		
+			for (int i=0; i<n; i++){
+				aux1[i] = a[i];
+			}
+			for (int i=0; i<n2; i++){
+				aux2[i] = a[n+i];
+			}
+			
+		
+		
+			mym (aux1, result_1);
+			mym (aux2, result_2);
+			
+			
+			if (result_1[0]>result_2[0])
+				result[0] = result_1[0];
+			else
+				result[0] = result_2[0];
+			
+			if (result_1[1]<result_2[1])
+				result[1] = result_1[1];
+			else
+				result[1] = result_2[1];
+			
+		}else {
+		
+			result[0] = a[0];
+			result[1] = a[0];
+		}
+	
+	}
+ 
+}
---
+++ src/dyv/busquedaNumero.java
@@ -0,0 +1,33 @@
+package dyv;
+
+class busquedaNumero {
+	public static int buscarIndice(int v[], int inicio, int fin) {
+		// System.out.println ("Entramos con: "+(inicio)+" "+fin);
+		if (inicio == fin) {
+			if (v[inicio] == inicio)
+				return inicio;
+			else
+				return -1;
+		} else {
+			int mitad = (inicio + fin) / 2;
+			if (v[mitad] == mitad) {
+				return mitad;
+			} else if (v[mitad] > mitad) {
+				return buscarIndice(v, inicio, mitad);
+			} else {
+				return buscarIndice(v, mitad + 1, fin);
+			}
+		}
+
+	}
+
+	public static void main(String args[]) {
+		int arrayEntrada[] = { -1, 1, 3, 4, 6, 20, 22, 24 }; // Este es el array
+																// de elementos
+
+		int elemento = buscarIndice(arrayEntrada, 0, arrayEntrada.length - 1);
+		System.out.println("Posicion buscada: " + elemento);
+
+	}
+
+}
