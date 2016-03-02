a49e0d71300c58ad3ad50431cb4009b97940319a
rivara <ricardo.v.ramirez@gmail.com>
2016-03-02 17:32:48 +0100

    nueva version

--- src/dyv/Hoja4E2.java
+++ src/dyv/Hoja4E2.java
@@ -1,7 +1,8 @@
 package dyv;
 
 public class Hoja4E2 {
-	private static int OrdenPos(int numeros[], int ini, int fin) {
+	
+	/*private static int OrdenPos(int numeros[], int ini, int fin) {
 		// caso base
 				if (ini == fin) {
 					return 0;
@@ -22,16 +23,44 @@ public class Hoja4E2 {
 					return 0;
 				}
 				
-			}
+			}*/
 
+    public static int buscarDesordenado (int v[], int inicio, int fin) {
+	if (inicio==fin) { 
+		return -1;
+	}else if (inicio+1==fin) { //Vector de dos posiciones 
+		if (v[fin]<v[inicio]) {
+			return fin;
+		} else {
+			return -1;
+		}
+	}else {
+		int mitad = (inicio + fin) /2;
+		if (v[mitad]> v[mitad+1]) {
+			return mitad+1;
+		}else if (v[mitad]<v[mitad-1]){
+			return mitad;
+		}else {
+			int posicion = buscarDesordenado (v, inicio, mitad-1);
+			if (posicion==-1) {
+				posicion = buscarDesordenado (v, mitad+1, fin);
+			}
+			return -1;
+		}
+	}
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				// llamada principal
-				int arrayEntrada[] = {1,2,3,4,5};
+			/*	int arrayEntrada[] = {1,2,3,4,5};
 				int ordenado = OrdenPos(arrayEntrada, 1, arrayEntrada.length-1);
 				System.out.println("si sale 0 esta ordenado");
 				System.out.println("");
-				System.out.println("esta ordenado" + ordenado);
+				System.out.println("esta ordenado" + ordenado);*/
+	//   int arrayEntrada[]={1,2,3,4,5,0,7,8,9,10,11}; //Este es el array de elementos 
+	    	int arrayEntrada[]={2,4,6,8,16,12,18,20,22,24}; //Este es el array de elementos 
+	     	int posicion=buscarDesordenado (arrayEntrada, 0, arrayEntrada.length-1);
+	    	System.out.println ("Elemento desordenado en: "+posicion);
 
 	}
 }
--- src/dyv/Hoja4E3.java
+++ src/dyv/Hoja4E3.java
@@ -1,7 +1,7 @@
 package dyv;
 
 public class Hoja4E3 {
-	private static String MatrizTipo(int numeros[][], int ini, int fin) {
+	/*private static String MatrizTipo(int numeros[][], int ini, int fin) {
 		// caso base
 				if (ini == fin) {
 					return "neutra";
@@ -17,21 +17,76 @@ public class Hoja4E3 {
 								&& (numeros[medio] < numeros[medio + 1])) {
 									 System.out.println("punto de desorden"+medio);
 									 return "restativa";
-					}*/
+					}
 						
 					return "sumamtiva";
 				}
 				
-			}
+			}*/
+	 public static char analizarMatriz (int[][] m) { 
+		   return analizarDyV (m, 0, m.length-1, 0, m[0].length-1);
+		  }
+
+		  private static char analizarDyV
+		                           (int[][] m,
+		                            int filaInf, int filaSup,
+		                            int colInf, int colSup) {
+		      if (filaInf==filaSup && colInf==colSup) {
+		         if (colInf==0 && colSup==0)
+		            return '0';
+		         else if (m[filaInf][colInf]==filaInf+colInf)
+		            return 's';
+		         else if (m[filaInf][colInf]==filaInf-colInf)
+		            return 'r';
+		         else
+		            return 'n';
+		      }
+		      else if (filaInf==filaSup) {
+		         int colMedia  = (colInf+colSup) / 2;
+		         char r1 = analizarDyV  (m, filaInf, filaSup,
+		                                    colInf,colMedia);
+		         char r2 = analizarDyV (m, filaInf, filaSup,
+		                                    colMedia+1, colSup);
+		         return (r1=='0' ? r2 : (r1==r2) ? r1 : 'n');
+		      }
+		      else if (colInf==colSup) {
+		         int filaMedia  = (filaInf+filaSup) / 2;
+		         char r1 = analizarDyV (m, filaInf, filaMedia,
+		                                   colInf, colSup);
+		         char r2 = analizarDyV (m, filaMedia+1, filaSup,
+		                                   colInf, colSup);
+		         return (r1=='0' ? r2 : (r1==r2) ? r1 : 'n');
+		      }
+		      else {
+		         int filaMedia = (filaInf+filaSup) / 2;
+		         int colMedia  = (colInf+colSup) / 2;
+		         char r1 = analizarDyV (m, filaInf, filaMedia,
+		                                   colInf, colMedia);
+		         char r2 = analizarDyV (m, filaInf, filaMedia,
+		                                   colMedia+1, colSup);
+		         char r3 = analizarDyV (m, filaMedia+1, filaSup,
+		                                   colInf, colMedia);
+		         char r4 = analizarDyV (m, filaMedia+1, filaSup,
+		                                   colMedia+1, colSup);
+		         return ((r1==r2) && (r1==r3) && (r1==r4)? r1 : 'n');
+		      } 
+		  }
+		  
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				// llamada principal
-				int arrayEntrada[][] = {{1,2,3,4,5},{1,2,3,4}};
+			/*	int arrayEntrada[][] = {{1,2,3,4,5},{1,2,3,4}};
 				String ordenado = MatrizTipo(arrayEntrada, 1, arrayEntrada.length-1);
 				System.out.println("si sale 0 esta ordenado");
 				System.out.println("");
-				System.out.println("esta ordenado" + ordenado);
+				System.out.println("esta ordenado" + ordenado);*/
+	//   int arrayEntrada[]={1,2,3,4,5,0,7,8,9,10,11}; //Este es el array de elementos 
+	    //int arrayEntrada[][]={{0,1,2},{1,2,3}}; //Este es el array de elementos 
+	    int arrayEntrada[][]= {{0,-1,-2},{1,0,-1}};
+	    char r=analizarMatriz (arrayEntrada);
+	    System.out.println ("Tipo: "+r);   
 
 	}
 
