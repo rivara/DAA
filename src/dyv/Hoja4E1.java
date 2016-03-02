a105f1e9fa6bdf93002f98f049691ef34035ba7d
rivara <ricardo.v.ramirez@gmail.com>
2016-03-02 17:14:35 +0100

    mergeSort

--- src/dyv/Hoja4E1.java
+++ src/dyv/Hoja4E1.java
@@ -1,7 +1,7 @@
 package dyv;
 
 public class Hoja4E1 {
-	private static boolean Orden(int numeros[], int ini, int fin) {
+/*	private static boolean Orden(int numeros[], int ini, int fin) {
 	// caso base
 			if (ini == fin) {
 				return true;
@@ -25,15 +25,47 @@ public class Hoja4E1 {
 			}
 			return false;
 			
-		}
+		}*/
+	 public static int estaOrdenado (int v[], int inicio, int fin) {
+			if (inicio==fin) {
+				return -1;
+			}else if (inicio+1==fin) {
+				if (v[inicio]<=v[fin]) {
+					return -1;
+				}else {
+					return inicio;
+				}
+			}else {
+				int mitad = (inicio + fin) /2;
+				
+				if ((v[mitad]<v[mitad-1]) || (v[mitad+1]<v[mitad])) 
+					return mitad;
+				else {
+					int pos_1 =  estaOrdenado (v, inicio, mitad);
+					int pos_2 =  estaOrdenado (v, mitad+1, fin);
+					if (pos_1>pos_2)
+						return pos_1;
+					else
+						return pos_2;
+				}
+			}
+		 }
 //falla en la segunda mitad
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				// llamada principal
+		/*		// llamada principal
 				int arrayEntrada[] = {1,4,5,7,20,11};
 				boolean ordenado = Orden(arrayEntrada, 1, arrayEntrada.length-1);
 				System.out.println("esta ordenado: " + ordenado);
 
+	}*/
+int arrayEntrada[]={1,2,3,8,6,23,23,24}; //Este es el array de elementos 
+		
+		int posicion_erronea = estaOrdenado (arrayEntrada, 0, arrayEntrada.length-1);
+		if (posicion_erronea==-1)
+			System.out.println ("Vector Ordenado");
+		else
+			System.out.println ("No esta ordenado: "+posicion_erronea);
 	}
 
 }
---
+++ src/dyv/MergeSort.java
@@ -0,0 +1,64 @@
+package dyv;
+
+public class MergeSort {
+	
+
+
+
+    public static void main(String args[]){
+       int arrayEntrada[]={321, 123, 213, 234, 1, 4, 5, 6}; //Este es el array de elementos que vamos a ordenar
+
+       mergeSort(arrayEntrada); //llamada al metodo mergeSort 
+       for (int i=0;i < arrayEntrada.length;i++){ //Este bucle imprime el contenido del array
+          System.out.print(arrayEntrada[i]+" ");
+       }//fin del for
+     }//fin del main
+     
+    public static void mergeSort( int a[ ]){
+      
+        mergeSort( a, 0, a.length - 1 );
+    }
+    private static void mergeSort( int a[ ], int left, int right ){
+        if( left < right )
+        {
+            int center = ( left + right ) / 2;
+            mergeSort( a, left, center );
+            mergeSort( a, center + 1, right );
+
+            //Ordena las dos partes anteriores para fusionar los dos vectores
+            merge( a, left, center + 1, right );
+        }
+    }
+
+    private static void merge( int a[ ],int leftPos, int rightPos, int rightEnd){
+  	int tmpArray[] = new int[ a.length ];
+
+        int leftEnd = rightPos - 1;
+        int tmpPos = leftPos;
+        int numElements = rightEnd - leftPos + 1;
+
+        // Main loop
+        while( leftPos <= leftEnd && rightPos <= rightEnd ){
+            if( a[ leftPos ]<( a[ rightPos ] ) ){
+                tmpArray[ tmpPos ] = a[ leftPos ];
+                tmpPos++;
+                leftPos++;
+            }
+            else{
+                tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+            }
+        }
+        while( leftPos <= leftEnd ){    // Copy rest of first half
+            tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+        }
+        while( rightPos <= rightEnd ){  // Copy rest of right half
+            tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+        }
+        // Copy TmpArray back
+        for( int i = 0; i < numElements; i++, rightEnd-- ){
+            a[ rightEnd ] = tmpArray[ rightEnd ];
+        }
+    }
+}//class MergeSort
+
+
