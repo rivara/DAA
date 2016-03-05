9f19d56d5169cdea55789379d2f8e138998e03ed
rivara <ricardo.v.ramirez@gmail.com>
2016-03-05 13:38:42 +0100

    Ejercicio hoja5

--- src/dyv/Hoja5E1.java
+++ src/dyv/Hoja5E1.java
@@ -9,27 +9,55 @@ el vector con los elementos duplicados eliminados. Por ejemplo, para una entrada
 Java que implemente una estrategia Divide y vencerás y que no supere el coste O( n log n ).*/
 public class Hoja5E1 {
 
-		public static int repetidos(int v[], int inicio, int fin,ArrayList<String> aux) {
+		public static int repetidos(int v[], int inicio, int fin,int aux[],int cont) {
 			if (inicio == fin) {
-				return  v[inicio];
-			
-			} else {
+				return  v[inicio];	
+			}else {
+				//recorro todo el array con mitad si aparece en otro sitio
 				int mitad = (inicio + fin) / 2;
-				int pos_1 = repetidos(v, inicio, mitad,aux);
-				int pos_2 = repetidos(v, mitad + 1, fin,aux);
-				if!(aux.contains(o))
-					aux.add(pos_1);
+			    
 				
-			}          
+			
+				int pos_1 = repetidos(v, inicio, mitad,aux,cont);
+				int pos_2 = repetidos(v, mitad+1, fin,aux,cont);
+			// si pos_1 es 0 nos encontramos en la primera posicion	
+			if (pos_1==0){		
+					//miro por un lado
+				 System.out.println(pos_1+"o"+mitad);
+					 if((v[mitad]==pos_1)&&(mitad!=0)){		
+						 System.out.println(pos_1+"se repite en la posicion "+mitad);
+						 aux[cont]=pos_1;
+						 cont++;
+					 }
+					 return pos_1;
+				}else{ 
+				//miro por el otro lado 	 
+					 if((v[mitad]==pos_2)&&(mitad!=v.length)){
+						 System.out.println(pos_2+"se repite en la posicion "+mitad);
+						 aux[cont]=pos_2;
+						 cont++;
+					 }
+					 return pos_2;
+				}
+			
+			}
 		}
-
+		
+		
+				
+		
+	    
 		
 
 	public static void main(String[] args) {
-		int arrayEntrada[] = { 4, 2, 8, 10, 20, 23, 24,25,21 }; // Este es el array
-		ArrayList<String> AList = new ArrayList<String>();
-		System.out.println(repetidos(arrayEntrada, 0,arrayEntrada.length - 1,AList));
-		
+		int arrayEntrada[] = {2,2,2,2,7,7,11}; // Este es el array
+		int array2[]=new int[arrayEntrada.length];
+		repetidos(arrayEntrada, 0,arrayEntrada.length - 1,array2,0);
+	
+	//compruebo todas las posiciones del array crado	
+	 for(int aux=0 ;aux<array2.length;aux++){
+		 System.out.print(array2[aux]);
+	 }
 	}
 
 	
