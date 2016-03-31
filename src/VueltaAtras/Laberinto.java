53f247d19e9cdc7f7ae113b191c713b9a394989a
rivara <ricardo.v.ramirez@gmail.com>
2016-03-31 20:35:37 +0200

    Nueva version

---
+++ src/VueltaAtras/Laberinto.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VueltaAtras;
+
+import java.util.Scanner;
+public class Laberinto {
+    private static boolean Resuelve (int paso, int fila_actual, int columna_actual, String [][] tablero, int dimension, int[] incrX, int[] incrY) {
+        boolean exito = false;
+        for (int intento=0; intento<4 && !exito; intento++) {
+            int nuevaF = fila_actual + incrX[intento];
+            int nuevaC = columna_actual + incrY[intento];
+            
+            if ((nuevaF>=0 && nuevaF<dimension) && (nuevaC>=0 && nuevaC<dimension)) {
+                if (tablero[nuevaF][nuevaC].equals("S")) {
+                    exito=true;
+                }else if (tablero[nuevaF][nuevaC].equals(" ")) {
+                    String numero = Integer.toString (paso);
+                    tablero[nuevaF][nuevaC] = numero;
+                    exito = Resuelve (paso+1, nuevaF, nuevaC , tablero, dimension, incrX, incrY);
+                    if (!exito) {
+                        tablero[nuevaF][nuevaC] =  " ";
+                    }
+               }
+                
+            }
+             
+         }
+      return exito;
+   }
+
+    private static void imprimir (String [][] tablero) {
+        for (int i=0; i<tablero.length; i++) {
+            for (int j=0; j<tablero.length; j++)
+                System.out.print (tablero[i][j]+"  ");
+         System.out.println();
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Dame la dimension del tablero MxM :");
+        String linea = leer.nextLine();
+        int dimension = Integer.parseInt(linea);
+    
+        String[][] tablero = new String[dimension][dimension];
+        int[] incrX = new int[] { 1,-1,0, 0};
+        int[] incrY = new int[] { 0, 0,1, -1};
+
+        for (int i= 0; i< dimension; i++)
+            for (int j=0; j< dimension; j++)
+                tablero[i][j] = " ";
+        
+        tablero[0][0] = "E";
+        tablero[2][2] = "M";
+        tablero[dimension-1][dimension-1] = "S";
+    
+        boolean exito = Resuelve (1, 0, 0, tablero, dimension, incrX, incrY);
+        if (exito)
+            imprimir (tablero);
+        else
+            System.out.println("No hay solucion");
+    }
+}
---
+++ src/VueltaAtras/SaltoDelCaballo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VueltaAtras;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jvergara
+ */
+public class SaltoDelCaballo {
+    private static boolean Resuelve (int paso, int fila_actual, int columna_actual, int[][] tablero, int dimension, int[] incrX, int[] incrY) {
+        boolean exito = false;
+        for (int intento=0; intento<8 && !exito; intento++) {
+            int nuevaF = fila_actual + incrX[intento];
+            int nuevaC = columna_actual + incrY[intento];
+            
+            if ((nuevaF>=0 && nuevaF<dimension) && (nuevaC>=0 && nuevaC<dimension)) {
+                if (tablero[nuevaF][nuevaC] == 0) {
+                    tablero[nuevaF][nuevaC] = paso;
+                    if (paso==dimension*dimension)
+                        exito = true;
+                    else {
+                        exito = Resuelve (paso+1,nuevaF, nuevaC , tablero, dimension, incrX, incrY);
+                        if (!exito)
+                            tablero[nuevaF][nuevaC] = 0;
+                    }
+               }
+            }
+      
+             
+         }
+      return exito;
+   }
+   private static void imprimir (int[][] tablero) {
+      for (int i=0; i<tablero.length; i++) {
+         for (int j=0; j<tablero.length; j++)
+            System.out.print (tablero[i][j]+"  ");
+         System.out.println();
+      }
+   }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Dame la dimension del tablero MxM (Ejemplo 5):");
+        String linea = leer.nextLine();
+        int dimension = Integer.parseInt(linea);
+    
+        int[][] tablero = new int[dimension][dimension];
+        int[] incrX = new int[] {-1,  1,  2, 2, 1, -1, -2, -2};
+        int[] incrY = new int[] {-2, -2, -1, 1, 2,  2,  1, -1};
+
+        tablero[0][0] = 1;
+        boolean exito = Resuelve (2, 0, 0, tablero, dimension, incrX, incrY);
+        if (exito)
+            imprimir (tablero);
+        else
+            System.out.println("No hay solucion");
+    }
+}
